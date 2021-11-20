a69230912dea023b226f859767939ba1a9fb60cf
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-11-20 03:05:23 +0100

    Creating the form html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  registerForm?: FormGroup;
   title = 'reactive-forms';
+
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(){
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: [],
+        lastName : [],
+        email : [],
+        password: [],
+        confirmPassword: [],
+        acceptTerms:[]
+      }
+    )
+  }
 }
