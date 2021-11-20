321a57ed232e1031c5a32f9aac7bd6f974a2d27d
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-11-20 22:21:14 +0100

    completing register reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { passworCheker } from './validators/password-checker';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,36 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class AppComponent implements OnInit{
   registerForm?: FormGroup;
   title = 'reactive-forms';
+  submit: boolean = false;
 
   constructor(private formBuilder:FormBuilder){}
 
   ngOnInit(){
     this.registerForm = this.formBuilder.group(
       {
-        firstName: [],
-        lastName : [],
-        email : [],
-        password: [],
-        confirmPassword: [],
-        acceptTerms:[]
-      }
+        firstName: ["", Validators.required],
+        lastName : ["", Validators.required],
+        email : ["", Validators.required, Validators.email],
+        password: ["", Validators.required],
+        confirmPassword: ["", Validators.required],
+        acceptTerms:["", Validators.required]
+      },{Validators : passworCheker('password','confirmPassword')}
     )
   }
+
+  onSubmit(){
+    this.submit = true;
+    if(this.registerForm?.invalid){
+      return;
+    }
+
+    console.table(this.registerForm?.value);
+    console.table(this.registerForm)
+  }
+
+  onReset(){
+    this.submit=false;
+    this.registerForm?.reset();
+  }
+
 }
